426dba659533505e7e9aded6f26655830811bbe1
Jonathan <jono_95@yahoo.com>
2022-01-25 13:19:12 -0500

    commit tree 966c4d29684cb7e8a6ab238f8f2379f6b1d922f1

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(number1, number2) {
+    return (number1 + number2);
+}
+
+function subtract(a, b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(a, b) {
+    return (a / b);
+}
+
+function increment(n) {
+    return (++n);
+}
+
+function decrement(n) {
+    return (--n);
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
